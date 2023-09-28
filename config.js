c9d119ec24b6b3cc54306a1e99936f8d171cd200
Yael Adar <yaeladar315@gmail.com>
2023-09-29 02:32:06 +0300

    updated url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://47ef40b6-88f9-48e0-ae0b-10734ecc3cd5.serverhub.tripleten-services.com"
+    API_URL: "https://b6747acc-998d-420f-8451-48f2c1230919.serverhub.tripleten-services.com"
 }
